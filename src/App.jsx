5d42973e42681d1d0d6dc3c3022e58e4b8eb5711
Dominic Menzel (Manni) <144007490+Cymanni@users.noreply.github.com>
2024-03-21 09:09:25 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  const name = 'World'
+
+  return (
+    <>
+    <h1 className='text-3xl underline text-center font-bold'>
+      Hello </h1>
+    </>
+  )
+}
+
+export default App
